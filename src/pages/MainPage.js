b56c98476d457e3ea0676e11b03204e0f61bff78
Charlie Tsai <charlie.tsai@getac.com.tw>
2024-11-21 15:20:48 +0800

    add/del member button (without functionality)

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -9,6 +9,10 @@ function MainPage() {
     const navigate = useNavigate();
     const [families, setFamilies] = useState([]);
 
+    const [isDialogVisible, setIsDialogVisible] = useState(null);
+    const [dialogType, setDialogType] = useState("");
+
+
     // Fetch data from Firebase
     useEffect(() => {
         const fetchData = async () => {
@@ -33,6 +37,30 @@ function MainPage() {
         navigate("/second");
     };
 
+    const handleFamilyCheckboxChange = (familyIndex, memberIndex, stateKey) => {
+        const updatedFamily = [...families];
+        updatedFamily[familyIndex]["members"][memberIndex][stateKey] = !updatedFamily[familyIndex]["members"][memberIndex][stateKey];
+        console.log(updatedFamily[familyIndex]["members"][memberIndex][stateKey])
+        setFamilies(updatedFamily);
+    };
+
+    const onCancelDialog = () => {
+        setIsDialogVisible(null);
+        setDialogType("");
+    }
+
+    const onConfirmDialog = (inputName, familyTitle, selectedGender, selectPopupType) => {
+        setIsDialogVisible(null);
+        setDialogType("");
+
+        if (selectPopupType === "Add") {
+            console.log("Add " + inputName + " " + familyTitle + " " + selectedGender)
+        }
+        else if (selectPopupType === "Del") {
+            console.log("Del " + inputName + " " + familyTitle + " " + selectedGender)
+        }
+    }
+
     return (
         <div style={{ textAlign: "center", marginTop: "50px", backgroundColor: '#fdf1df'}}>
 
@@ -43,9 +71,30 @@ function MainPage() {
                     // Family Block
                     <div>
                         {/* Family Name Block */}
-                        <div className="block">
-                            <div>
-                                <div className="title">{familyData.title}</div>
+                        <div className="block" style={{ flexDirection: 'row', display: 'flex' }} >
+                            <div className="title">{familyData.title}</div>
+                            <div className="iconContainer">
+                                <button 
+                                    className="iconButton"
+                                    onClick={() => { setIsDialogVisible(familyIndex); setDialogType("Add"); console.log("Add Clicked!"); }}
+                                >
+                                <img
+                                    src={require('../assets/images/addMember.png')}
+                                    alt="Add Member"
+                                    className="iconImage"
+                                />
+                                </button>
+
+                                <button 
+                                    className="iconButton"
+                                    onClick={() => { setIsDialogVisible(familyIndex); setDialogType("Del"); console.log("Del Clicked!"); }}
+                                >
+                                <img
+                                    src={require('../assets/images/delMember.png')}
+                                    alt="Delete Member"
+                                    className="iconImage"
+                                />
+                                </button>
                             </div>
                         </div>
 
@@ -69,7 +118,31 @@ function MainPage() {
                                 {familyData.members.map((member, memberIndex) => (
                                     <div className="familyRow familyRowBackgound">
                                         <div className="familyNameCol">
-                                            <text className="text">{member.name}</text>
+                                            <div className="text">{member.name}</div>
+                                        </div>
+                                        <div className="familyCheckboxCol">
+                                            <input
+                                                type="checkbox"
+                                                className="checkbox"
+                                                checked={member.arriveState}
+                                                onChange={() => handleFamilyCheckboxChange(familyIndex, memberIndex, "arriveState")}
+                                            />
+                                        </div>
+                                        <div className="familyCheckboxCol">
+                                            <input
+                                                type="checkbox"
+                                                className="checkbox"
+                                                checked={member.groupState}
+                                                onChange={() => handleFamilyCheckboxChange(familyIndex, memberIndex, "groupState")}
+                                            />
+                                        </div>
+                                        <div className="familyCheckboxCol">
+                                            <input
+                                                type="checkbox"
+                                                className="checkbox"
+                                                checked={member.mealState}
+                                                onChange={() => handleFamilyCheckboxChange(familyIndex, memberIndex, "mealState")}
+                                            />
                                         </div>
                                     </div>
                                 ))}
@@ -78,8 +151,6 @@ function MainPage() {
                     </div>
                 ))}
             </div>
-
-
 
             <button onClick={goToSecondPage} style={{ padding: "10px 20px", fontSize: "16px" }}>
                 Go to Second Page
